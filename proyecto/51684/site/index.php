cbf2b584c63636d956d5c513f98af2c4c12299df
Daniel <vanchocaballero@gmail.com>
2024-04-17 12:51:10 +0200

    17/04

--- proyecto/51684/site/index.php
+++ proyecto/51684/site/index.php
@@ -39,6 +39,7 @@
   session_start();
   if (isset($_SESSION['usuario'])) {
     echo '<script>var nombre = "' . $_SESSION['usuario'] . '"; var inicio=true;</script>';
+    echo 'hiehdiehiheidheihd';
   } else {
     echo '<script>var inicio=false;</script>';
   }
