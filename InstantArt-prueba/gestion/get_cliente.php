2dbc4441eb958f609f8948ce77b74b09e96b28e6
Ivan_marino <vanchocaballero@gmail.com>
2024-10-22 11:59:34 +0200

    mejorando

--- InstantArt-prueba/gestion/get_cliente.php
+++
@@ -1,9 +0,0 @@
-<?php
-require 'utilidades_gestion.php';
-
-session_start();
-
-if (isset($_GET['id_cliente'])) {
-    $id_cliente = intval($_GET['id_cliente']);
-    echo cliente_id($id_cliente);
-}
--- InstantArt-prueba/php/almacenar_id_update.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-
-$id = $_POST['id'];
-
-
-$_SESSION['id_update'] = $id;
-
-echo $_SESSION['id_update'];
